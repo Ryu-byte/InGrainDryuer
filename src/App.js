0c771915167b0f4122affb96da7f10b2a52e2e99
Dmitry <dkoval@webjox.ru>
2021-03-29 20:53:28 +0300

    Add resizing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Header} from "./components/Header";
 import {Content} from "./components/Content";
-import React, { useState } from "react";
 
 function App() {
     return (
